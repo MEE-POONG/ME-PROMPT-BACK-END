59b51b927aec0cc2dd37dcb64fe640431d86dc35
devilzeros <devilzeros@outlook.co.th>
2022-12-15 14:47:54 +0700

    est

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,26 +1,26 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOMClient from "react-dom/client"; // Import the ReactDOMClient package
 import Router from "next/router";
 import PageChange from "../components/PageChange/PageChange";
 import App from "next/app";
 import Head from "next/head";
 import SSRProvider from 'react-bootstrap/SSRProvider';
 import 'assets/scss/index.scss';
-Router.events.on("routeChangeStart", (url) => {
-  const root = createRoot(document.getElementById("page-transition"));
-  document.body.classList.add("body-page-transition");
-  root.render(<PageChange path={url} />);
-});
-Router.events.on("routeChangeComplete", () => {
-  const root = createRoot(document.getElementById("page-transition"));
-  root.unmount();
-  document.body.classList.remove("body-page-transition");
-});
-Router.events.on("routeChangeError", () => {
-  const root = createRoot(document.getElementById("page-transition"));
-  root.unmount();
-  document.body.classList.remove("body-page-transition");
-});
+// Router.events.on("routeChangeStart", (url) => {
+//   const root = ReactDOMClient.createRoot(document.getElementById("page-transition"));
+//   document.body.classList.add("body-page-transition");
+//   root.render(<PageChange path={url} />);
+// });
+// Router.events.on("routeChangeComplete", () => {
+//   const root = ReactDOMClient.createRoot(document.getElementById("page-transition"));
+//   root.unmount();
+//   document.body.classList.remove("body-page-transition");
+// });
+// Router.events.on("routeChangeError", () => {
+//   const root = ReactDOMClient.createRoot(document.getElementById("page-transition"));
+//   root.unmount();
+//   document.body.classList.remove("body-page-transition");
+// });
 export default class MyApp extends App {
   componentDidMount() {
     let comment = document.createComment(`
@@ -47,13 +47,7 @@ export default class MyApp extends App {
     return (
       <SSRProvider>
         <React.Fragment>
-          <Head>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1, shrink-to-fit=no"
-            />
-            <title>Notus NextJS by Creative Tim</title>
-          </Head>
+
           <Layout>
             <Component {...pageProps} />
           </Layout>
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,7 +13,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <title>Notus NextJS by Creative Tim</title>
+        </Head>
         <body className="bg-body">
           <div id="page-transition"></div>
           <Main />
