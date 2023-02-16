950c4bb057de5d2b4540214b67bb0033baa1d2bc
devilzeros <Devilzeros@outlook.co.th>
2023-02-16 15:42:03 +0700

    est

---
+++ pages/api/workwith/[id].js
@@ -0,0 +1,66 @@
+const prisma = require('../prisma');
+
+export default async function handler(req, res) {
+    const { method } = req
+    switch (method) {
+        case 'GET':
+            try {
+                const data = await prisma.workwith.findFirst({
+                    where: {
+                        id: req.query.id
+                    }
+                });
+                
+                res.status(200).json(data)
+            } catch (error) {
+                res.status(400).json({ success: false })
+            }
+            break
+        case 'PUT':
+            try {
+                await prisma.workwith.update({
+                    where: {
+                        id: req.query.id
+                    },
+                    data: {
+                        username: req.body.username,
+                        password: req.body.password,
+                        firstname: req.body.firstname,
+                        lastname: req.body.lastname,
+                        facebook: req.body.facebook,
+                        line: req.body.line,
+                        instagram: req.body.instagram,
+                        addressOne: req.body.addressOne,
+                        addressTwo: req.body.addressTwo,
+                        subDistrict: req.body.subDistrict,
+                        district: req.body.district,
+                        city: req.body.city,
+                        postalCode: req.body.postalCode,
+                        positionId: req.body.positionId,
+                        updatedBy: req.body.updatedBy,
+                    }
+                })
+                
+                res.status(201).json({ success: true })
+            } catch (error) {
+                res.status(400).json({ success: false })
+            }
+            break
+        case 'DELETE':
+            try {
+                await prisma.workwith.delete({
+                    where: {
+                        id: req.query.id
+                    }
+                });
+                
+                res.status(204).end()
+            } catch (error) {
+                res.status(400).json({ success: false })
+            }
+            break
+        default:
+            res.setHeader('Allow', ['GET', 'POST'])
+            res.status(405).end(`Method ${method} Not Allowed`)
+    }
+}
--- pages/api/workwith/index.js
+++ pages/api/workwith/index.js
@@ -8,39 +8,50 @@ export default async function handler(req, res) {
                 let page = +req.query.page || 1;
                 let pageSize = +req.query.pageSize || 10;
                 const data = await prisma.$transaction([
-                    prisma.workWithSkill.count(),
-                    prisma.workWithSkill.findMany({
+                    prisma.workwith.count() || 0,
+                    prisma.workwith.findMany({
                         skip: (page - 1) * pageSize,
                         take: pageSize,
+                        include: { Position: true },
                     })
                 ])
                 const totalPage = Math.ceil(data[0] / pageSize);
                 res.status(200).json({ data: data[1], page, pageSize, totalPage })
-
             } catch (error) {
-                console.log(error);
-
                 res.status(400).json({ success: false })
             }
             break
         case 'POST':
             try {
-                const nameCheck = await prisma.workWithSkill.findMany({
-                    where: { workWithId: req.body.workWithId, skillId: req.body.skillId }
+                const nameCheck = await prisma.workwith.findMany({
+                    where: { firstname: req.body.firstname, lastname: req.body.lastname }
                 });
                 if (nameCheck.length === 0) {
-                    await prisma.workWithSkill.create({
+                    await prisma.workwith.create({
                         data: {
-                            workWithId: req.body.workWithId,
-                            skillId: req.body.skillId,
+                            username: req.body.username,
+                            password: req.body.password,
+                            firstname: req.body.firstname,
+                            lastname: req.body.lastname,
+                            facebook: req.body.facebook,
+                            line: req.body.line,
+                            instagram: req.body.instagram,
+                            addressOne: req.body.addressOne,
+                            addressTwo: req.body.addressTwo,
+                            subDistrict: req.body.subDistrict,
+                            district: req.body.district,
+                            city: req.body.city,
+                            postalCode: req.body.postalCode,
+                            positionId: req.body.positionId,
                             createdBy: req.body.createdBy,
                         }
                     });
                     res.status(201).json({ success: true });
                 } else {
-                    res.status(400).json({ success: false, message: 'มีบันทึกสกิลนี้แล้ว' });
+                    res.status(400).json({ success: false, message: 'มีสกิล ' + req.body.firstname + ' ' + req.body.lastname + ' แล้ว' });
                 }
             } catch (error) {
+                
                 res.status(400).json({ success: false })
             }
             break
@@ -49,3 +60,6 @@ export default async function handler(req, res) {
             res.status(405).end(`Method ${method} Not Allowed`)
     }
 }
+
+
+
