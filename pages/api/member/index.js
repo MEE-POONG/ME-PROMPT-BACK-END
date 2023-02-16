3a69e7e6e504bd26836108613fcd2d8b77b06a9a
devilzeros <Devilzeros@outlook.co.th>
2023-02-16 13:02:28 +0700

    est

--- pages/api/member/index.js
+++ pages/api/member/index.js
@@ -23,27 +23,33 @@ export default async function handler(req, res) {
             break
         case 'POST':
             try {
-                await prisma.member.create({
-                    data: {
-                        username: req.body.username,
-                        password: req.body.password,
-                        firstname: req.body.firstname,
-                        lastname: req.body.lastname,
-                        facebook: req.body.facebook,
-                        line: req.body.line,
-                        instagram: req.body.instagram,
-                        addressOne: req.body.addressOne,
-                        addressTwo: req.body.addressTwo,
-                        subDistrict: req.body.subDistrict,
-                        district: req.body.district,
-                        city: req.body.city,
-                        postalCode: req.body.postalCode,
-                        positionId: req.body.positionId,
-                        createdBy: req.body.createdBy,
-                        
-                    }
-                })
-                res.status(201).json({ success: true })
+                const nameCheck = await prisma.member.findMany({
+                    where: { firstname: req.body.firstname, lastname: req.body.lastname }
+                });
+                if (nameCheck.length === 0) {
+                    await prisma.member.create({
+                        data: {
+                            username: req.body.username,
+                            password: req.body.password,
+                            firstname: req.body.firstname,
+                            lastname: req.body.lastname,
+                            facebook: req.body.facebook,
+                            line: req.body.line,
+                            instagram: req.body.instagram,
+                            addressOne: req.body.addressOne,
+                            addressTwo: req.body.addressTwo,
+                            subDistrict: req.body.subDistrict,
+                            district: req.body.district,
+                            city: req.body.city,
+                            postalCode: req.body.postalCode,
+                            positionId: req.body.positionId,
+                            createdBy: req.body.createdBy,
+                        }
+                    });
+                    res.status(201).json({ success: true });
+                } else {
+                    res.status(400).json({ success: false, message: 'มีสกิล ' + req.body.firstname + ' ' + req.body.lastname + ' แล้ว' });
+                }
             } catch (error) {
                 console.log(error);
                 res.status(400).json({ success: false })
