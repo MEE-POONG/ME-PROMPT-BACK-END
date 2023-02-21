c4ba7915ab0311ee748a16146ecdb001444335ed
devilzeros <devilzeros@outlook.co.th>
2023-02-21 16:26:16 +0700

    est

---
+++ pages/api/blog/[id].js
@@ -0,0 +1,63 @@
+const prisma = require('../prisma');
+
+export default async function handler(req, res) {
+    const { method } = req
+    switch (method) {
+        case 'GET':
+            try {
+                const data = await prisma.blog.findFirst({
+                    where: {
+                        id: req.query.id
+                    }
+                });
+
+                res.status(200).json(data)
+            } catch (error) {
+                res.status(400).json({ success: false })
+            }
+            break
+        case 'PUT':
+            try {
+                await prisma.blog.update({
+                    where: {
+                        id: req.query.id
+                    },
+                    data: {
+                        title: req.body.title,
+                        subTitle: req.body.subTitle,
+                        subDetail: req.body.subDetail,
+                        detailOne: req.body.detailOne,
+                        detailTwo: req.body.detailTwo,
+                        detailThree: req.body.detailThree,
+                        listOne: req.body.listOne,
+                        listTwo: req.body.listTwo,
+                        listThree: req.body.listThree,
+                        start: req.body.start,
+                        end: req.body.end,
+                        updatedBy: req.body.updatedBy,
+                    }
+                })
+
+                res.status(201).json({ success: true })
+            } catch (error) {
+                res.status(400).json({ success: false })
+            }
+            break
+        case 'DELETE':
+            try {
+                await prisma.blog.delete({
+                    where: {
+                        id: req.query.id
+                    }
+                });
+
+                res.status(204).end()
+            } catch (error) {
+                res.status(400).json({ success: false })
+            }
+            break
+        default:
+            res.setHeader('Allow', ['GET', 'POST'])
+            res.status(405).end(`Method ${method} Not Allowed`)
+    }
+}
---
+++ pages/api/ourwork/index.js
@@ -0,0 +1,53 @@
+const prisma = require('../prisma');
+
+export default async function handler(req, res) {
+    const { method } = req
+    switch (method) {
+        case 'GET':
+            try {
+                let page = +req.query.page || 1;
+                let pageSize = +req.query.pageSize || 10;
+                const data = await prisma.$transaction([
+                    prisma.ourWork.count(),
+                    prisma.ourWork.findMany({
+                        skip: (page - 1) * pageSize,
+                        take: pageSize,
+                    })
+                ])
+                const totalPage = Math.ceil(data[0] / pageSize);
+                res.status(200).json({ data: data[1], page, pageSize, totalPage })
+            } catch (error) {
+                res.status(400).json({ success: false })
+            }
+            break
+        case 'POST':
+            try {
+
+                await prisma.ourWork.create({
+                    data: {
+                        title: req.body.title,
+                        subTitle: req.body.subTitle,
+                        subDetail: req.body.subDetail,
+                        detailOne: req.body.detailOne,
+                        detailTwo: req.body.detailTwo,
+                        detailThree: req.body.detailThree,
+                        listOne: req.body.listOne,
+                        listTwo: req.body.listTwo,
+                        listThree: req.body.listThree,
+                        start: req.body.start,
+                        end: req.body.end,
+                        createdBy: req.body.createdBy,
+                    }
+                });
+                res.status(201).json({ success: true });
+            } catch (error) {
+                console.log(error);
+                res.status(400).json({ success: false });
+            }
+            break;
+
+        default:
+            res.setHeader('Allow', ['GET', 'POST'])
+            res.status(405).end(`Method ${method} Not Allowed`)
+    }
+}
