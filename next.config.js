a6980134161d968851e391fe8edf9612331b9228
devilzeros <devilzeros@outlook.co.th>
2023-03-04 17:55:30 +0700

    est

--- next.config.js
+++ next.config.js
@@ -1,20 +1,8 @@
 /** @type {import('next').NextConfig} */
-const intercept = require("intercept-stdout")
-
-function interceptStdout(text) {
-  if (text.includes("Duplicate atom key")) {
-    return ""
-  }
-  return text
-}
-
-if (process.env.NODE_ENV === "development") {
-  intercept(interceptStdout)
-}
-
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone'
+  experimental: {
+    appDir: true,
+  },
 }
 
 module.exports = nextConfig
